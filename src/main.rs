eeb018720e95df4cf4c6b714f60f0b0eaa429167
g1tblame <silvercrocus@proton.me>
2025-08-20 22:23:18 +0200

    implemented syscall's body struct and wrapped registers

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,15 @@ use sysnames::Syscalls;
 use exec;
 use std::env;
 
+struct SyscallBody {
+    ret: u64,
+    first_arg: u64,
+    second_arg: u64,
+    third_arg: u64,
+    name: String,
+    num: u64,
+}
+
 fn fork_init() {
     match unsafe{fork()} {
         Ok(ForkResult::Parent{child}) => {
@@ -35,22 +44,29 @@ fn check_args_len(exec_args: usize) -> bool {
 
 fn handle_syscall(child_pid: &Pid) {
     let regs = ptrace::getregs(*child_pid).unwrap();
-    if regs.rax == -ENOSYS as u64 {
+
+    let syscall = SyscallBody {
+        ret: regs.rax,
+        first_arg: regs.rdi,
+        second_arg: regs.rsi,
+        third_arg: regs.rdx,
+        num: regs.orig_rax,
+        name: Syscalls::name(regs.orig_rax).unwrap().to_uppercase(),
+    };
+
+    if syscall.ret == -ENOSYS as u64 {
         // it means that we are entering syscall so do nothing
         ();
     }
     else {
-        let syscall_name = Syscalls::name(regs.orig_rax).unwrap().to_uppercase();
-        let ret = regs.rax;
-        println!("{}({:#x}, {:#x}, {:#x}) = {}", syscall_name, regs.rdi, regs.rsi, regs.rdx, ret);
+        println!("{}({:#x}, {:#x}, {:#x}) = {}", syscall.name, syscall.first_arg, syscall.second_arg, syscall.third_arg, syscall.ret);
     }
 }
 
 fn tracee_init() {
     ptrace::traceme().expect("failed to set TRACEME flag");
     let cli_args: Vec<String> = env::args().collect();
-    let _ = exec::Command::new(cli_args[1].clone()).
-        exec();
+    let _ = exec::Command::new(cli_args[1].clone()).exec();
 }
 
 fn tracer_init(child_pid: &Pid) {
@@ -60,7 +76,7 @@ fn tracer_init(child_pid: &Pid) {
         ptrace::syscall(*child_pid, None);
         match waitpid(*child_pid, None) {
             Ok(WaitStatus::Exited(_, _)) => {
-                println!("process was finished!");
+                println!("child process was finished!");
                 break;
             },
             Ok(WaitStatus::Stopped(pid_t, sig_t)) => {
