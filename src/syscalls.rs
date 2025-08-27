0b9adbf52e2672b71cb2aae1bc6f75053146b2e9
cr0cus <silvercrocus@proton.me>
2025-08-27 21:41:29 +0200

    refactored openat syscall

--- src/syscalls.rs
+++ src/syscalls.rs
@@ -49,37 +49,7 @@ pub fn brk_syscall(child_pid: &Pid, syscall: &mut SyscallBody) {
 
 pub fn openat_syscall(child_pid: &Pid, syscall: &mut SyscallBody) {
      let openat_addr = syscall.second_arg as *mut c_void;
-     let mut words_count = 0;
-     let word_size = 8;
-     let mut stack_string = String::new();
-
-     'read: loop {
-         let mut raw_bytes: Vec<u8> = vec![];
-         let openat_addr = unsafe {openat_addr.offset(words_count)};
-
-
-         let mut stack_data: c_long = 0;
-         match ptrace::read(*child_pid, openat_addr) {
-             Ok(res) => stack_data = res,
-             Err(_) => break 'read,
-         }
-
-          raw_bytes.write_i64::<LittleEndian>(stack_data).unwrap_or_else(|err| {
-              panic!("Failed to write {} as i64 LittleEndian: {}", stack_data, err);
-          });
-
-          for b in raw_bytes {
-              if b != 0 {
-                  stack_string.push(b as char);
-              } else {
-                  break 'read;
-              }
-          }
-          words_count += word_size;
-     }
-     syscall.second_arg_string = stack_string;
-     syscall.args_count_flag = 2;
-    // syscall.print();
+     syscall.second_arg_string = read_stack_data(child_pid, openat_addr);
      if(syscall.first_arg == 4294967196) {
          println!("{}(AT_FDCWD, {}, {:#x}) = {:#x}", syscall.name, syscall.second_arg_string, syscall.third_arg, syscall.ret);
      }
